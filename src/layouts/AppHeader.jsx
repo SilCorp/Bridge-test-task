e854cb42c5ca2f5298e57deef939bf543d459a9d
Alexander Silchenko <alex.silcorp@gmail.com>
2022-05-29 21:00:01 +0300

    refactor: set app bar position

--- src/layouts/AppHeader.jsx
+++ src/layouts/AppHeader.jsx
@@ -12,12 +12,17 @@ function AppHeader() {
   };
 
   return (
-    <AppBar>
+    <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Bridge
         </Typography>
-        <Button onClick={onLogOut} color="inherit">
+        <Button
+          onClick={onLogOut}
+          color="inherit"
+          variant="text"
+          sx={{ textTransform: 'none' }}
+        >
           SignOut
         </Button>
       </Toolbar>
