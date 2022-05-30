1159d81f37b1a572d3a84a4af6e7ec00530ae843
Alexander Silchenko <alex.silcorp@gmail.com>
2022-05-31 01:52:50 +0300

    add PlayingField

---
+++ src/components/PlayingField/index.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, CircularProgress } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import PlayingCard from '../PlayingCard';
+import {
+  getTwoCardsAsync,
+  loseBet,
+  selectCards,
+  selectGameStatus,
+  setGameStatus,
+  winBet,
+} from '../../store/AppStore/AppSlice';
+import gameStatus from '../../constants/gameStatus-constants';
+import WelcomeTextBlock from './components/WelcomeTextBlock';
+import compareCards from '../../helpers/compareCards';
+
+function PlayingField() {
+  const [selectedCard, setSelectedCard] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+  const currentGameStatus = useSelector(selectGameStatus);
+  const isUserMakingDecision = currentGameStatus === gameStatus.preparation;
+  const isGameProcessing = currentGameStatus === gameStatus.processing;
+  const isGameFinished = currentGameStatus === gameStatus.finished;
+  const isGameInitialized = currentGameStatus === gameStatus.initialized;
+  const dispatch = useDispatch();
+  const [firstCard, secondCard] = useSelector(selectCards);
+  const firstCardImage = firstCard?.image;
+  const secondCardImage = secondCard?.image;
+
+  const cardNumberWin = compareCards(firstCard?.value, secondCard?.value) === 1 ? 1 : 2;
+  const hasUserWon = cardNumberWin === selectedCard;
+
+  useEffect(() => {
+    if (isGameFinished) {
+      if (hasUserWon) {
+        dispatch(winBet());
+      } else {
+        dispatch(loseBet());
+      }
+    }
+  }, [dispatch, hasUserWon, isGameFinished]);
+
+  useEffect(() => {
+    setFlipped(isGameFinished);
+  }, [isGameFinished]);
+
+  useEffect(() => {
+    let promise;
+    if (isGameProcessing) {
+      promise = dispatch(getTwoCardsAsync());
+    }
+
+    return () => {
+      if (promise) {
+        promise.abort();
+      }
+    };
+  }, [dispatch, isGameProcessing]);
+
+  const onFirstCardSelect = () => {
+    if (isUserMakingDecision) {
+      setSelectedCard(1);
+      dispatch(setGameStatus(gameStatus.processing));
+    }
+  };
+
+  const onSecondCardSelect = () => {
+    if (isUserMakingDecision) {
+      setSelectedCard(2);
+      dispatch(setGameStatus(gameStatus.processing));
+    }
+  };
+
+  const onStartGameClick = () => {
+    dispatch(setGameStatus(gameStatus.preparation));
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      padding="30px"
+      sx={{ backgroundColor: '#e5e5e5' }}
+    >
+      <WelcomeTextBlock hasUserWon={hasUserWon} />
+      <Box
+        display="flex"
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          display="inline-flex"
+          alignItems="center"
+        >
+          <PlayingCard
+            isFrontSide={flipped}
+            sx={{ margin: '20px' }}
+            cardFaceSrc={firstCardImage}
+            onClick={onFirstCardSelect}
+          />
+          {isUserMakingDecision && (
+            <Button
+              variant="contained"
+              onClick={onFirstCardSelect}
+            >
+              Слева
+            </Button>
+          )}
+        </Box>
+
+        {isGameProcessing && (
+          <CircularProgress size={80} />
+        )}
+
+        {(isGameFinished || isGameInitialized) && (
+          <Button
+            onClick={onStartGameClick}
+            variant="outlined"
+          >
+            {isGameFinished && 'Играть снова!'}
+            {isGameInitialized && 'Играть'}
+          </Button>
+        )}
+
+        <Box
+          display="inline-flex"
+          alignItems="center"
+        >
+          {isUserMakingDecision && (
+            <Button
+              variant="contained"
+              onClick={onSecondCardSelect}
+            >
+              Справа
+            </Button>
+          )}
+          <PlayingCard
+            isFrontSide={flipped}
+            sx={{ margin: '20px' }}
+            cardFaceSrc={secondCardImage}
+            onClick={onSecondCardSelect}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default PlayingField;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from '@mui/material';
+import { useDispatch } from 'react-redux';
 import AppHeader from '../layouts/AppHeader';
 import ShowBalance from '../components/ShowBalance';
+import { getNewDeckIdAsync } from '../store/AppStore/AppSlice';
+import PlayingField from '../components/PlayingField';
 
 function MainPage() {
+  const dispatch = useDispatch();
+
+  // Fetch new deck
+  useEffect(() => {
+    dispatch(getNewDeckIdAsync());
+  }, [dispatch]);
+
   return (
     <Container>
       <AppHeader />
       <main>
         <ShowBalance />
+        <PlayingField />
       </main>
     </Container>
   );
