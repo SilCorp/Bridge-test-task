13a897fdddbb60154ef6b54d679944315d44f3b7
Alexander Silchenko <alex.silcorp@gmail.com>
2022-05-30 23:06:13 +0300

    add gameStatus-constants

---
+++ src/constants/gameStatus-constants.js
@@ -0,0 +1,9 @@
+const gameStatus = {
+  initialized: 'initialized',
+  ready: 'ready',
+  preparation: 'preparation',
+  processing: 'processing',
+  finished: 'finished',
+};
+
+export default gameStatus;
