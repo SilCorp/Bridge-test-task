43471e05b866f9639cde34c546d9d25e7191d075
Alexander Silchenko <alex.silcorp@gmail.com>
2022-05-31 21:01:31 +0300

    add login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  Box, Container, TextField, Typography,
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
 import { MAIN_PAGE } from '../constants/route-constants';
 import useAuth from '../services/Auth/useAuth';
 
@@ -10,21 +14,79 @@ function LoginPage() {
 
   const from = location.state?.from?.pathname || MAIN_PAGE;
 
-  const onLogin = () => {
-    signIn(() => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async (event) => {
+    try {
+      setError(false);
+      setLoading(true);
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const login = data.get('login');
+      const password = data.get('password');
+      await signIn(login, password);
       navigate(from, { replace: true });
-    });
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div>
-      <button
-        type="button"
-        onClick={onLogin}
+    <Container
+      maxWidth="xs"
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        component="form"
+        onSubmit={onLogin}
       >
-        Login
-      </button>
-    </div>
+        <TextField
+          margin="dense"
+          required
+          fullWidth
+          label="Login"
+          name="login"
+          autoComplete="login"
+          error={error}
+          autoFocus
+        />
+        <TextField
+          margin="dense"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          error={error}
+        />
+        {error && (
+          <Typography>
+            Имя пользователя или пароль введены не верно
+          </Typography>
+        )}
+        <LoadingButton
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3 }}
+          loading={loading}
+        >
+          Sign In
+        </LoadingButton>
+      </Box>
+    </Container>
   );
 }
 
